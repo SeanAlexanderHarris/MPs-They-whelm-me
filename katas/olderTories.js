e63ac5b83eebbc1bfdffd73b414c7dcec6e278d9
SeanAlexanderHarris <sean.alexander.harris.29@googlemail.com>
2018-03-19 22:54:15 +0000

    completed tests and solution for olderTories

--- katas/olderTories.js
+++ katas/olderTories.js
@@ -1,3 +1,13 @@
-function olderTories() { }
+function olderTories(arr) {
+  return arr.reduce((acc, mpObj) => {
+    if (mpObj.office !== undefined) {
+      mpObj.office.forEach(officeObj => {
+        let dateSlice = parseInt(officeObj.from_date.slice(0, 5));
+        dateSlice < 2015 ? acc.push(mpObj.name) : 0;
+      })
+    }
+    return acc;
+  }, []);
+}
 
 module.exports = { olderTories }
--- spec/olderTories.spec.js
+++ spec/olderTories.spec.js
@@ -3,7 +3,7 @@ const expect = require("chai").expect;
 const MPDATA = require("../MPs.json")
 const fs = require('fs');
 
-describe("olderTories", () => {
+describe.only("olderTories", () => {
   let mpData;
   before(function (done) {
     fs.readFile('./MPs.json', 'utf8', (err, data) => {
@@ -14,36 +14,85 @@ describe("olderTories", () => {
 
   it("returns an empty array when passed an empty array", () => {
     const input = [];
-    const actual = olderTories;
+    const actual = olderTories(input);
     const expected = []
     expect(actual).to.eql(expected);
   });
 
   it("returns an array of one name when passed an array of one valid MP", () => {
-    const input = [mpData[]];
-    const actual = olderTories;
-    const expected = []
+    const input = [mpData[23]];
+    const actual = olderTories(input);
+    const expected = ["Robert Buckland"]
     expect(actual).to.eql(expected);
   });
 
   it("returns an empty array when passed an array of one invalid name", () => {
-    const input = [];
-    const actual = olderTories;
+    const input = [mpData[7]];
+    const actual = olderTories(input);
     const expected = []
     expect(actual).to.eql(expected);
   });
 
   it("returns an array of one name when passed an array of one valid name and one invalid name", () => {
-    const input = [];
-    const actual = olderTories;
-    const expected = []
+    const input = [mpData[22], mpData[23]];
+    const actual = olderTories(input);
+    const expected = ["Robert Buckland"]
     expect(actual).to.eql(expected);
   });
 
   it("returns an array of the correct names when passed the entire data set", () => {
-    const input = [];
-    const actual = olderTories;
-    const expected = []
+    const input = mpData;
+    const actual = olderTories(input);
+    const expected = [
+      'Robert Buckland',
+      'Alan Campbell',
+      'Jonathan Edwards',
+      'Jonathan Edwards',
+      'Jonathan Edwards',
+      'Hywel Williams',
+      'Eleanor Laing',
+      'Lindsay Hoyle',
+      'Mark Tami',
+      'Jeremy Wright',
+      'Nigel Dodds',
+      'Nick Gibb',
+      'Gareth Thomas',
+      'Graham Brady',
+      'John Bercow',
+      'Steve Pound',
+      'Robert Buckland',
+      'Alan Campbell',
+      'Jonathan Edwards',
+      'Jonathan Edwards',
+      'Jonathan Edwards',
+      'Hywel Williams',
+      'Eleanor Laing',
+      'Lindsay Hoyle',
+      'Mark Tami',
+      'Jeremy Wright',
+      'Nigel Dodds',
+      'Nick Gibb',
+      'Gareth Thomas',
+      'Graham Brady',
+      'John Bercow',
+      'Steve Pound',
+      'Robert Buckland',
+      'Alan Campbell',
+      'Jonathan Edwards',
+      'Jonathan Edwards',
+      'Jonathan Edwards',
+      'Hywel Williams',
+      'Eleanor Laing',
+      'Lindsay Hoyle',
+      'Mark Tami',
+      'Jeremy Wright',
+      'Nigel Dodds',
+      'Nick Gibb',
+      'Gareth Thomas',
+      'Graham Brady',
+      'John Bercow',
+      'Steve Pound'
+    ]
     expect(actual).to.eql(expected);
   });
 
